aea568f531aa7658d4f2e2b297b3e20bf7f7fcfb
max <max@DESKTOP-URFOHJT>
2017-03-19 11:10:54 +0800

    sss

--- CompSystem/src/com/cs/service/article/ArticleService.java
+++ CompSystem/src/com/cs/service/article/ArticleService.java
@@ -1,14 +1,12 @@
 package com.cs.service.article;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import com.cs.pojo.Article;
 @Service
 public interface ArticleService {
-	public List<Article> findByType(int type );
-	public Map findArticleByType(int type );
+	List<Article> findByType(int type);
 
 }
