a93da27a525d76b0241a7ccf3e3d9210ea9d6ca3
rabitBaby <545306388@qq.com>
2017-03-19 14:51:21 +0800

    222

--- CompSystem/src/com/cs/controller/login/LoginController.java
+++ CompSystem/src/com/cs/controller/login/LoginController.java
@@ -35,7 +35,7 @@ public class LoginController {
 	 * 判断登录的方法
 	 * @param username  用户名
 	 * @param password  密码
-	 * @param role  角色111changed by hw
+	 * @param role  角色222changed by hw
 	 * @return
 	 */
 	@ResponseBody
