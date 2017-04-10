e59cfe2a05f70696edea3a10050663a39a179ed0
rabitBaby <545306388@qq.com>
2017-04-11 01:18:04 +0800

    文件上传

---
+++ CompSystem/WebRoot/studentIndex.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://"
+			+ request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<base href="<%=basePath%>">
+
+<title>My JSP 'index.jsp' starting page</title>
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+<meta http-equiv="description" content="This is my page">
+
+</head>
+
+<body>
+	<form name="Form2" action="<%=path%>/student/saveMaterial" method="post"  enctype="multipart/form-data">
+	<h1>使用spring mvc提供的类的方法上传文件</h1>
+	<input type="file" name="file">
+	<input type="submit" value="upload"/>
+	</form>
+</body>
+</html>
--- CompSystem/src/com/cs/controller/student/StudentController.java
+++ CompSystem/src/com/cs/controller/student/StudentController.java
@@ -1,36 +1,28 @@
 package com.cs.controller.student;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-import com.cs.pojo.Awards;
-import com.cs.pojo.Budget;
-import com.cs.pojo.Competition;
 import com.cs.pojo.Conditions;
-import com.cs.pojo.Department;
-import com.cs.pojo.Groups;
-import com.cs.pojo.Hours;
-import com.cs.pojo.Material;
-import com.cs.pojo.Schedule;
 import com.cs.pojo.Student;
-import com.cs.pojo.Teacher;
 import com.cs.service.condition.ConditionService;
 import com.cs.service.student.StudentService;
-import com.cs.service.teacher.TeacherService;
+
 
 
 @Controller
@@ -76,6 +68,38 @@ public class StudentController {
 	@RequestMapping("/getDissatisfyConditions")
 	public List<Conditions> getDissatisfyConditions(){
 		return studentService.getDissatisfyConditions(1, 1);
+	}
+	
+	/**
+	 * 1.3个人信息：证书、学生证等的上传
+	 * @param request
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
+	@ResponseBody
+	@RequestMapping("/saveMaterial")
+	public void saveMaterial(HttpServletRequest request) throws IllegalStateException, IOException{
+		//将当前上下文初始化给CommonsMultipartResolver（多部分解析器）
+		CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		//检查表单是否有enctype="multipart/form-data"属性
+		if(multipartResolver.isMultipart(request)){
+			//将request变成多部分request
+			MultipartHttpServletRequest multiRquest=(MultipartHttpServletRequest)request;
+			//获取multiRquest的所有文件名
+			Iterator iter=multiRquest.getFileNames();
+			while (iter.hasNext()) {
+				//一次遍历所有文件
+				MultipartFile file=multiRquest.getFile(iter.next().toString());
+				if(file!=null){//文件不为空
+					//下载的位置-----------------------------------------
+					String path="E:/temp/"+file.getOriginalFilename();
+					//上传
+					file.transferTo(new File(path));
+				}
+			}
+		
+		}
 	}
 	
 	/**
