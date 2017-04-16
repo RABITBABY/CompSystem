7162bb913cdfda4aefa7c67130216c4eb32b365d
max <max@DESKTOP-URFOHJT>
2017-04-16 16:49:49 +0800

    文章Mapper

--- CompSystem/src/com/cs/dao/article/ArticleMapper.java
+++ CompSystem/src/com/cs/dao/article/ArticleMapper.java
@@ -1,19 +1,33 @@
 package com.cs.dao.article;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.cs.pojo.Article;
 
 public interface ArticleMapper {
-    int deleteByPrimaryKey(Integer articleid);
-
-    int insert(Article record);
-
-    int insertSelective(Article record);
-
-    Article selectByPrimaryKey(Integer articleid);
-
-    int updateByPrimaryKeySelective(Article record);
-
-    int updateByPrimaryKeyWithBLOBs(Article record);
-
-    int updateByPrimaryKey(Article record);
+	
+	/**
+	 * 获取某类型的一定数量文章
+	 * @param map
+	 * @return
+	 */
+	List<Map> getMtypeList(Map map);
+	/**
+	 * 获取所有该类型的文章数量
+	 * @param type
+	 * @return
+	 */
+	int getTotal(String type);
+	
+	/**
+	 * 根据文章id获取文章的详细内容
+	 * @param id
+	 * @return
+	 */
+	Map findById(int id);
+	
+	
+	
 }
