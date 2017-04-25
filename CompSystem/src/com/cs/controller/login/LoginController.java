4755ba388ee50dfa4691eadd2d8ca129c03dab83
max <max@DESKTOP-URFOHJT>
2017-04-26 01:11:08 +0800

    登录2

--- CompSystem/src/com/cs/controller/login/LoginController.java
+++ CompSystem/src/com/cs/controller/login/LoginController.java
@@ -3,6 +3,7 @@ package com.cs.controller.login;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -128,6 +129,94 @@ public class LoginController {
 		return resultMap;
 	}
 
+	@ResponseBody
+	@RequestMapping(value = "/signIn")
+	public Map login(String account,String password, String role, HttpServletRequest request){
+		
+		Map<String , Object> resultMap=new HashMap<String, Object>();
+		String stateCode="1";
+		String info="";
+		String userId="";
+		String userName="";
+		String roleNum="";//1--教师，2--审批员，3--学生，4--管理员
+		//显示传递的数据
+		System.out.println(password+"---"+account+"--"+role);
+		
+		
+		//获取前台页面的信息---先判断角色色
+		if("teacher".equals(role)){//教师
+			
+			Teacher teacher=teachImpl.selectByTeacherNo(Integer.parseInt(account));
+			
+			if(teacher!=null){
+				if(password.equals(teacher.getPassword())){
+					System.out.println(teacher);
+					request.getSession().setAttribute("user", teacher); 
+					if(teacher.getExaminer()==0){//教师
+						roleNum="1";
+					}else{//审批员
+						roleNum="2";
+					}
+					userId=String.valueOf(teacher.getTeacherno());
+					userName=teacher.getTeachername();
+					info= "success";
+				}else{
+					stateCode="0";
+					info= "wrongPassword";
+				}
+			}else{
+				stateCode="0";
+				info= "wrongAccount";
+			}
+			
+		}else if("student".equals(role)){//学生
+			Student stu =stuImpl.selectByNo(Integer.parseInt(account));
+			if(stu!=null){
+				if(password.equals(stu.getPassword())){
+					roleNum="3";
+					userId=String.valueOf(stu.getStudentno());
+					userName=stu.getStudentname();
+					info= "success";
+				}else{
+					stateCode="0";
+					info= "wrongPassword";
+				}
+			} else{
+				stateCode="0";
+				info= "wrongAcount";
+			}
+		}else if("admin".equals(role)){//管理员
+			Administer admin=adminImpl.selectByID(account);
+			if(admin!=null){
+				if(password.equals(admin.getPassword())){
+					roleNum="4";
+					userId=admin.getAdminno();
+					userName=admin.getAdminname();
+					info= "success";
+				}else{
+					stateCode="0";
+					info= "wrongPassword";
+				}
+			}else{
+				stateCode="0";
+				info= "wrongAcount";
+			}
+		}
+		resultMap.put("stateCode", stateCode);
+		resultMap.put("info", info);
+		resultMap.put("userId", userId);
+		resultMap.put("userName", userName);
+		resultMap.put("roleNum", roleNum);
+		System.out.println("resultMap"+resultMap);
+		if("1".equals(stateCode)){
+			
+			request.getSession().setAttribute("loginInfo", resultMap);
+		}
+		return resultMap;
+	}
+
+	
+	
 	/**
 	 * 获取登录用户的信息
 	 * @param session
