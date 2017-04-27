dda75877737cac6576e45d712bf712fd101b44cc
rabitBaby <545306388@qq.com>
2017-04-27 21:45:58 +0800

    数据库修改

--- CompSystem/src/com/cs/controller/websocket/WebSocketController.java
+++ CompSystem/src/com/cs/controller/websocket/WebSocketController.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.cs.pojo.Greeting;
-import com.cs.pojo.HelloMessage;
 import com.cs.pojo.Message;
 import com.sun.org.apache.regexp.internal.recompile;
 
@@ -32,49 +30,5 @@ public class WebSocketController {
         return null;
     }
 
-    @MessageMapping("/admin")
-    @SendTo("/topic/admin")
-    public Greeting admin(HelloMessage message) throws Exception {
-        Thread.sleep(3000); // simulated delay
-        return new Greeting("Hello, " + message.getName() + "!");
-    }
-
-    @MessageMapping("/sb")
-    @SendTo("/topic/sb")
-    public Greeting sb(HelloMessage message) throws Exception {
-        Thread.sleep(3000); // simulated delay
-        return new Greeting("Hello, " + message.getName() + "!");
-    }
-
-    @MessageMapping("/themanager")
-    @SendTo("/topic/themanager")
-    public String AllMessage(String message) throws Exception {
-        return  "系统公告："+message;
-    }
-
-
-    @MessageMapping("/waiter")
-    @SendTo("/topic/waiter")
-    public String waiter(String message) throws Exception {
-        return "厨师提醒您：" + message+ "!";
-    }
-
-    @MessageMapping("/cooker")
-    @SendTo("/topic/cooker")
-    public String cooker(String message) throws Exception {
-        return "点餐员提醒您：" + message+ "!";
-    }
-
-    @MessageMapping("/cashier")
-    @SendTo("/topic/cashier")
-    public String cashier(String message) throws Exception {
-        return "点餐员提醒您：" + message+ "!";
-    }
-
-    @MessageMapping("/fresh")
-    @SendTo("/topic/fresh")
-    public String fresh() throws Exception {
-        return "刷新!";
-    }
-
+    
 }
